17d4f726f199377ab48e9310a86b0774c7da5f3f
mingz <305603665@qq.com>
2016-10-13 00:40:23 +0800

    init

---
+++ src/message/base_message.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+__author__ = 'zhaojm'
+
+
+class BaseMessage(object):
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
---
+++ src/message/qq.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+__author__ = 'zhaojm'
+
+from base_message import BaseMessage
+
+
+class QQMessage(BaseMessage):
+    def __init__(self):
+        BaseMessage.__init__(self)
+        pass
+
+    def run(self):
+        pass
